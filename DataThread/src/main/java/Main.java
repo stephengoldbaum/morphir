2f222761c674ff3e5259195871855e7859498d72
Stephen Goldbaum <129341+stephengoldbaum@users.noreply.github.com>
2024-06-09 21:40:55 +0000

    start for java support

---
+++ DataThread/src/main/java/Main.java
@@ -0,0 +1,52 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.File;
+
+import org.datathread.grammar._typespec.json_schema.Element;
+import javax.lang.model.element.Modifier;
+
+public class Main {
+    public static void main(String[] args) {
+        String json = null;
+        try {
+            System.out.println(new File(".").getAbsolutePath());
+            File baseDir = new File("DataThread/example/metastore");
+            String id = "element:person:active";
+            File file = new File(baseDir, resolveForID(id).getPath());
+
+            json = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            Element element = mapper.readValue(json, Element.class);
+            TypeSpec java = elementToJava(element);
+            System.out.println(java);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static TypeSpec elementToJava(Element element) {
+        TypeSpec java = TypeSpec.classBuilder(element.getName())
+            .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+            .build();
+
+        return java;
+    }
+
+    public static File resolveForID(String id) {
+        String[] parts = id.split(":");
+        return resolveFile(parts[0], parts[1], parts[2]);
+    }
+
+    public static File resolveFile(String schemaType, String domain, String name) {
+        return new File(domain, name + "." + schemaType + ".json");
+    }
+}
